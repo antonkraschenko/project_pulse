96a77803506907d38f4e5f08c3c57a4a711ce4bc
Anton <antonkraschenko@ukr.net>
2022-06-23 12:35:54 +0300

    converting min file in dist

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,18 @@ const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 
 // Static server
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
+
+    gulp.watch('src/*.html').on('change', browserSync.reload)
 });
 
 gulp.task('styles', function() {
@@ -28,13 +31,35 @@ gulp.task('styles', function() {
         grid: true
         }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() { 
-    gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'))
-    gulp.watch('src/*.html').on('change', browserSync.reload)
+    gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'))
+    gulp.watch('src/*.html').on('change', gulp.parallel('html'));
 });
 
-gulp.task('defoult', gulp.parallel('watch','server','styles'));
+gulp.task('html', function() {
+    return gulp.src("src/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src("src/js/**/*.js")
+    .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('fons', function() {
+    return gulp.src("src/fonts/**/*")
+    .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('images', function() {
+    return gulp.src("src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images"));
+});
+
+gulp.task('defoult', gulp.parallel('watch','server','styles', 'scripts', 'fons', 'images', 'html'));
